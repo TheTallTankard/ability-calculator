9b02ff5b67148f56d3c1f73e07526ac98e790909
Tyler Jass <jasstsg@gmail.com>
2022-12-03 19:58:48 -0500

    cost is calculated now too

--- src/scripts/AbilityScoreRow.js
+++ src/scripts/AbilityScoreRow.js
@@ -19,14 +19,14 @@ class AbilityScoreRow extends React.Component {
         this.setState((state) => ({score: parseInt(e.target.value)}));
         this.setState((state) => ({total: parseInt(e.target.value) + state.bonus}));
         this.setState((state) => ({modifier: parseInt(state.total / 2) - 5}));
-        console.log(this.state);
+        this.setState((state) => ({cost: this.scoreToCost(e.target.value)}));
     }
 
     handleBonusChange(e){
         this.setState((state) => ({bonus: parseInt(e.target.value)}));
         this.setState((state) => ({total: state.score + parseInt(e.target.value)}));
         this.setState((state) => ({modifier: parseInt(state.total / 2) - 5}));
-        console.log(this.state);
+        this.setState((state) => ({cost: this.scoreToCost(state.score)}));
     }
 
     render(){
@@ -35,13 +35,21 @@ class AbilityScoreRow extends React.Component {
                 <td>{this.props.ability}</td>
                 <td><input id="score" type="number" min="8" max="15" value={this.state.score} onChange={this.handleScoreChange}></input></td>
                 <td>+</td>
-                <td><input id="bonus" type="number" min="0" value={this.state.bonus} onChange={this.handleBonusChange}></input></td>
+                <td><input id="bonus" type="number" min="0" max="2" value={this.state.bonus} onChange={this.handleBonusChange}></input></td>
                 <td>=</td>
                 <td><span id="total"></span>{this.state.total}</td>
                 <td><span id="modifier"></span>{this.state.modifier}</td>
                 <td><span id="cost"></span>{this.state.cost}</td>
             </tr>
         )
+    }
+
+    scoreToCost(score){
+        switch(score){
+            case '15': return 9;
+            case '14': return 7;
+            default: return (score - 8);
+        }
     }
 }
 
