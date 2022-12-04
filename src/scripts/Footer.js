7c30b8c5261f6f8e60d2f77c9c3883a2418b1faf
Tyler Jass <jasstsg@gmail.com>
2022-12-03 20:44:02 -0500

    Updated title, favicon, and footer

--- src/scripts/Footer.js
+++ src/scripts/Footer.js
@@ -1,10 +1,12 @@
 import logo from '../logo.svg';
+import tankard from '../TheTallTankard_Zoomed_NoBackground.png';
 import '../styles/Footer.css';
 
 function Footer () {
     return (
         <div className="Footer">
-            Built with React <img src={logo} className="react-logo" alt="react logo" />
+            The Tall Tankard 
+            <img src={tankard} className="tankard-logo" alt="tankard logo" />
         </div>
     )
 }
