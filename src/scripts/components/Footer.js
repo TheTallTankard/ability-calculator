0458261f4fac24a741d5e0b15609c7bcaf83c6f3
Tyler Jass <jasstsg@gmail.com>
2022-12-07 15:58:22 -0500

    updated footer style and linked to homepage

--- src/scripts/components/Footer.js
+++ src/scripts/components/Footer.js
@@ -4,9 +4,19 @@ import '../../styles/Footer.css';
 function Footer () {
     return (
         <div className="Footer">
-            The Tall Tankard
-            <img src={tankard} className="tankard-logo flipped" alt="tankard logo" />
-            <img src={tankard} className="tankard-logo" alt="tankard logo" />
+            <div>
+                <strong><a href="https://thetalltankard.github.io">The Tall Tankard</a></strong>
+            </div>
+            <div className="last-updated">
+                Last updated 2022.12.07
+            </div>
+            <br/>
+            <br/>
+            <div>
+                <img src={tankard} className="tankard-logo flipped" alt="tankard logo" />
+                <img src={tankard} className="tankard-logo" alt="tankard logo" />
+            </div>
+            <br/>
         </div>
     )
 }
